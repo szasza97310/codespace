ba9da790db5b5050edfbe8810dbf4f406619f469
szasza97310 <143808062+szasza97310@users.noreply.github.com>
2023-09-01 15:07:56 +0000

    Fri, Sep 1, 2023, 3:07 PM +00:00

--- population/population.c
+++ population/population.c
@@ -5,13 +5,7 @@ int main(void)
 {
 int n;
 do
-{ n = get_int();
+{ n = get_int("Starting population size: ");
 }
 while (n < 9)
-
-    // TODO: Prompt for end size
-
-    // TODO: Calculate number of years until we reach threshold
-
-    // TODO: Print number of years
 }
